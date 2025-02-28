fe9cfb54800a7828e2775f271b72297e9628f278
me-coderaider <kashyappankajnsp@gmail.com>
2025-02-28 23:29:39 +0530

    GrabitWebParent for frontend and backend as module 2

---
+++ GrabitWebParent/src/main/java/com/grabit/GrabitWebParentApplication.java
@@ -0,0 +1,13 @@
+package com.grabit;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GrabitWebParentApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GrabitWebParentApplication.class, args);
+	}
+
+}
